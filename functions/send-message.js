aa5e793ba6acf81aeb377492f6e4668339108be9
Destroyer-attack <32365959+Destroyer-attack@users.noreply.github.com>
2024-06-26 20:57:36 +0530

    Initial1

---
+++ functions/send-message.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+ 
+
+const NODEKEY = process.env.NODE_KEY;
+const NODEPASS = process.env.NODE_PASS; 
+
+
+
+function convertQueryStringToJson(queryString) {
+  // Split the query string into key-value pairs
+  const pairs = queryString.split('&');
+
+  // Initialize an empty object to store the result
+  const result = {};
+
+  // Loop through each key-value pair
+  pairs.forEach(pair => {
+    // Split each pair into key and value
+    const [key, value] = pair.split('=');
+
+    // Decode the URL-encoded value
+    const decodedValue = decodeURIComponent(value);
+
+    // Assign the key-value pair to the result object
+    result[key] = decodedValue;
+  });
+
+  return result;
+}
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
+  }
+
+  try {
+    let data = convertQueryStringToJson(event.body);
+    console.log(data)
+    const { name, email, message } = data;
+    console.log('Received data:', { name, email, message });
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: "jayachandrands0120@gmail.com",
+        pass:  "qjzv wrsj tlet aces",
+      },
+    });
+
+    let mailOptions = {
+      from: email,
+      to: 'jayachandrands0120@gmail.com',
+      subject: 'New Contact Form Message',
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Email successfully sent!' }),
+    };
+  } catch (error) {
+    console.error('Error:', error.message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error sending email. Please try again later.' }),
+    };
+  }
+};
